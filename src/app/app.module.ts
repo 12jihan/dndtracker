aebefaeb21ec7ac97c589f15346953aca300393e
root <root@DemixGod-PC.localdomain>
2022-08-17 11:28:52 -0400

    added the shared module to help a bit more with the seperation of concerns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageModule } from './features/mainpage/mainpage.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MainpageModule } from './features/mainpage/mainpage.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainpageModule
+    MainpageModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
